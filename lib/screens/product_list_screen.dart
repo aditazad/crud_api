8ecefdb92707aba23e635db42a147caed5168a5a
aditazad <aditazad0008@gmail.com>
2023-11-01 22:17:39 +0600

    Initial commit

--- lib/screens/product_list_screen.dart
+++ lib/screens/product_list_screen.dart
@@ -1,10 +1,29 @@
 import 'package:crud_api/screens/add_new_product_screen.dart';
 import 'package:crud_api/widgets/product_item.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
-class ProductListScreen extends StatelessWidget {
+class ProductListScreen extends StatefulWidget {
   const ProductListScreen({super.key});
 
+  @override
+  State<ProductListScreen> createState() => _ProductListScreenState();
+}
+
+class _ProductListScreenState extends State<ProductListScreen> {
+
+  @override
+  void initState() {
+    getProductList();
+    super.initState();
+  }
+
+
+  void getProductList() async{
+    Response response = await get(Uri.parse('https://crud.teamrabbil.com/api/v1/ReadProduct'),);
+    print(response.statusCode);
+    print(response.body);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
